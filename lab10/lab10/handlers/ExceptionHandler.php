3219c250a2343591c64c0feb4f2269f498948bc1
mrbrain111 <ysolcev.nikita@nttek.ru>
2025-04-02 22:05:40 +0500

    Delete lab10/lab10 directory

--- lab10/lab10/handlers/ExceptionHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ExceptionHandler {
-    public static function handle(Throwable $exception) {
-        print("Произошла ошибка: {$exception -> getMessage()}<br>");
-        print("Файл: {$exception -> getFile()}<br>");
-        print("Строка: {$exception -> getLine()}<br>");
-    }
-}
